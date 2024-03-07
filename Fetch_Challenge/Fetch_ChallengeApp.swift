3b4cfe0d80b5461befe0631ab793d085a9eebb67
Khang Nguyen <36774417+DuyKhangTruong@users.noreply.github.com>
2024-03-07 12:01:52 -0800

    Initial Commit

---
+++ Fetch_Challenge/Fetch_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ChallengeApp.swift
+//  Fetch_Challenge
+//
+//  Created by Duy Khang Nguyen Truong on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
